88c31d72e39c269bdfb9ba725249d5849feb380e
o-star <uh04049@naver.com>
2021-02-02 18:15:25 +0900

    server client link up test

--- client/src/components/contents/IntroContents.js
+++ client/src/components/contents/IntroContents.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function IntroContents() {
+
+    const [apivalue, setApivalue] = useState(null);
+
+    useEffect(() => {
+        if (apivalue === null) {
+            fetch('/api')
+                .then((res) => res.text())
+                .then((restext) => setApivalue(restext))
+                .catch((err) => { console.log(err); })
+        }
+    })
+
     return (
         <div>
             Introduction section
+            <div>
+                API return value : {apivalue}
+            </div>
         </div>
     );
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router();
 
-router.get('/test', (req, res) => {
+router.get('/', (req, res) => {
+    console.log('client request test');
     res.send('test api');
 })
+
+module.exports = router;
