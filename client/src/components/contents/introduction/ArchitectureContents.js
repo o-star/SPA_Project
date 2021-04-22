becca72705be0538aee831ca9cef7eee79a7c4df
o-star <uh04049@naver.com>
2021-04-23 00:55:11 +0900

    architecture image update

--- client/src/components/contents/introduction/ArchitectureContents.js
+++ client/src/components/contents/introduction/ArchitectureContents.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-
-import 'assets/css/ArchitectureContents.css'
 import architectureimg from 'assets/image/SPA_Architecture.png'
 
 export default function ArchitectureContents(props) {
